9af92bb44a7e196e1837f6777df89cdd1d550b2e
PILCA Flavius <flavius.pilca@ext.europarl.europa.eu>
2020-11-26 13:36:44 +0100

    work on home page - build table and search box

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,18 @@
 import React, {useEffect, useState} from "react";
+import BootstrapTable from "react-bootstrap-table-next";
 import axios from "axios";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faLink} from "@fortawesome/free-solid-svg-icons";
+import {faLink, faTimes, faSearch} from "@fortawesome/free-solid-svg-icons";
 import {FETCH_API, SEARCH_API} from "../core/api";
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
-import BootstrapTable from "react-bootstrap-table-next";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+
 import paginationFactory from 'react-bootstrap-table2-paginator';
 import "./home.scss";
 
@@ -12,7 +20,8 @@ import "./home.scss";
 
 const Home = () => {
 
-    const [theNews, setThenews] = useState([]);
+    const [theNews, setTheNews] = useState([]);
+    const [filterName, setFilterName] = useState("");
 
     const columns = [
       {
@@ -42,7 +51,7 @@ const Home = () => {
         formatter: (cell, contact) => {
           return (
               <div className="d-flex justify-content-between">
-                <a href={contact.url} target="_blank">
+                <a href={contact.url} target="_blank" rel="noreferrer">
                   <FontAwesomeIcon style={{color: '#000'}} icon={faLink} />
                 </a>
               </div>
@@ -52,7 +61,7 @@ const Home = () => {
     ];
 
     const params = {
-        "country" : "us",
+        "country" : "de",
         "apiKey" : "cfc7d6168c29479ba18693c171134230"
     };
 
@@ -62,18 +71,23 @@ const Home = () => {
         return new Date(dateString).toLocaleDateString(undefined, options)
     };
 
-    const pagination = paginationFactory({
-        sizePerPage : 10,
-        sizePerPageList : [ {
-            text: '', value: 10
-        } ]
-    });
+    const paginationOptions = {
+        page: 1,
+        sizePerPage: 10,
+        lastPageText: '>>',
+        firstPageText: '<<',
+        nextPageText: '>',
+        prePageText: '<',
+        alwaysShowAllBtns: false,
+        hideSizePerPage: true,
+        hidePageListOnlyOnePage: true
+    };
 
     const getNews = (params) => {
         axios
             .get(FETCH_API, {params})
             .then(({data}) => {
-              setThenews(data.articles);
+              setTheNews(data.articles);
             })
     };
 
@@ -83,15 +97,69 @@ const Home = () => {
 
 
     return(<div className="home-page-container">
-        <BootstrapTable
-            keyField="content"
-            bootstrap4
-            remote
-            condensed
-            data={theNews}
-            columns={columns}
-            pagination={pagination}
-        />
+
+
+
+
+        <section className="content">
+            <Container>
+                <Card>
+                    <Card.Header>
+                        <Row>
+                            <Col>
+                                <InputGroup className="mt-0">
+                                    <FormControl
+                                        placeholder="search ..."
+                                        value={filterName}
+                                        onKeyPress={event => {
+                                            if (event.key === "Enter") {
+                                                //searchContactsByFilter(filterName, filterActive, filterOrganisational)
+                                            }
+                                        }}
+                                        onChange={(e) => {
+                                            setFilterName(e.target.value)
+                                        }}
+                                    />
+                                    {filterName && <Button
+                                        variant="light"
+                                        className="clean-form-btn"
+                                        onClick={(e) => {
+                                            e.currentTarget.parentElement.children[0].value = '';
+                                            setFilterName('');
+
+                                            //searchContactsByFilter(null, filterActive, filterOrganisational)
+                                        }}>
+                                        <FontAwesomeIcon style={{color: '#000'}} icon={faTimes} />
+                                    </Button>}
+                                    <InputGroup.Append>
+                                        <Button
+                                            variant="outline-secondary"
+                                            onClick={() => {
+                                                //searchContactsByFilter(filterName, filterActive, filterOrganisational)
+                                            }}
+                                        >
+                                            <FontAwesomeIcon style={{color: '#000'}} icon={faSearch} />
+                                        </Button>
+                                    </InputGroup.Append>
+                                </InputGroup>
+                            </Col>
+                        </Row>
+                    </Card.Header>
+                    <Card.Body>
+                        <BootstrapTable
+                            keyField="publishedAt"
+                            bootstrap4
+                            condensed
+                            data={theNews}
+                            columns={columns}
+                            pagination={paginationFactory(paginationOptions)}
+                        />
+                    </Card.Body>
+                    <Card.Footer>
+                    </Card.Footer>
+                </Card>
+            </Container>
+        </section>
     </div>)
 };
 
