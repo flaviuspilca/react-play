c4435f6b2fb8e8a1f38d83a25bf5f071e17958e2
PILCA Flavius <flavius.pilca@ext.europarl.europa.eu>
2020-11-30 12:40:44 +0100

    modal finishing up

--- src/pages/home.js
+++ src/pages/home.js
@@ -36,11 +36,11 @@ const NewsFullView = (props) => {
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <h4>by {props.data.author}</h4>
                 <p>{props.data.content}</p>
+                <span className="text-left font-italic">by: {props.data.author}</span>
             </Modal.Body>
             <Modal.Footer>
-                <Button onClick={props.onHide}>Close</Button>
+                <Button size={"sm"} onClick={props.onHide}>Close</Button>
             </Modal.Footer>
         </Modal>
     );
